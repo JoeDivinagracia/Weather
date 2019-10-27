65f847b4d271de8d544cc9daf5310f539c7bed8a
JoeDivinagracia <joediv789@gmail.com>
2019-10-27 00:35:00 -0400

    Removed the two JSON parsing functions and put them in their respective weather functions to cut down on class returns. Changed some return types of currentWeather() and forecastWeather(). Swapped the order of function definitions and class definitions to fix some issues.

--- src/includes/weather.h
+++ src/includes/weather.h
@@ -1,12 +1,6 @@
 #ifndef WEATHER_H
 #define WEATHER_H
 
-static size_t WriteMemoryCallback(void* contents, size_t size, size_t nmemb, void* userp);
-void getWeather(std::string location);
-void currentWeather(std::string URL);
-void forecastWeather(std::string URL);
-void parseJSON(char* chunkMemory);
-
 class MemoryChunk{
  private:
   char* memory;
@@ -28,17 +22,38 @@ class MemoryChunk{
   char* getMemory(){ return memory; }
 };
 
-class Weather{
+class CurrentWeather{
  private:
   std::string description;
-  double temperature;
+  double temperature, maxTemp, minTemp, windSpeed;
   
  public:
   void setDescription(std::string description){ this->description = description; }
-  void setTemp(double temperature){ this->temperature = temperature;}
+  void setTemp(double temperature){this->temperature = temperature;}
+  void setMinTemp(double minTemp){this->minTemp = minTemp;}
+  void setMaxTemp(double maxTemp){this->maxTemp = maxTemp;}
+  void setWindSpeed(double windSpeed){this->windSpeed = windSpeed;}
   
   std::string getDescription(){return description;}
   double getTemperature(){return temperature;}
+  double getMinTemp(){return minTemp;}
+  double getMaxTemp(){return maxTemp;}
+  double getWindSpeed(){return windSpeed;}
 };
+
+class ForecastWeather{
+ private:
+  std::string dateTime;
+
+ public:
+  void setDateTime(std::string dateTime){this->dateTime = dateTime;}
+
+  std::string getDateTime(){return dateTime;}
+};
+
+static size_t WriteMemoryCallback(void* contents, size_t size, size_t nmemb, void* userp);
+void getWeather(std::string location);
+CurrentWeather currentWeather(std::string URL);
+ForecastWeather forecastWeather(std::string URL);
 
 #endif
